a58108702c6cd715609f36db7ffb8003c9f7d8bb
Andriy Ostapchuk <andreyostap123@gmail.com>
2024-02-13 13:52:59 +0200

    TechPrakt2024Task0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Num1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Num2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number1 + number2;
+            Console.WriteLine($"Сума чисел {number1} та {number2} = {sum}.");
+        }
+    }
+}
